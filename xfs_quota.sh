1c46cdc4af70f6b892ae819efc20751c31974ace
malone12 <malone12@illinois.edu>
2024-02-14 23:40:31 -0600

    Adjusted XFS check for final

--- xfs_quota.sh
+++ xfs_quota.sh
@@ -3,12 +3,12 @@
 tfile=$(mktemp /tmp/xfs.XXXXXXX)
 path=""
 
-xfs_quota -x -c 'report' ${path}  > "${tfile}.block"
-xfs_quota -x -c 'report -i' ${path}  > "${tfile}.inode"
+sudo /usr/sbin/xfs_quota -x -c 'report' ${path}  > "${tfile}.block"
+sudo /usr/sbin/xfs_quota -x -c 'report -i' ${path}  > "${tfile}.inode"
 
-users=($(cat "${tfile}.block" | sed -n '/Group\ quota\ on/q;p' | awk '{print $1}' | tail -n +5 | xargs))
-cat "${tfile}.block" |  sed -n '/Group\ quota\ on/q;p' | tail -n +5 | awk '{print $1" "$2" "$3" "$4}' > "${tfile}.user_block"
-cat "${tfile}.inode" | sed -n '/Group\ quota\ on/q;p' | tail -n +5 | awk '{print $1" "$2" "$3" "$4}' > "${tfile}.user_inode"
+users=($(sed -n '/Group\ quota\ on/q;p' "${tfile}.block" | awk '{print $1}' | tail -n +5 | xargs))
+sed -n '/Group\ quota\ on/q;p' "${tfile}.block" | tail -n +5 | awk '{print $1" "$2" "$3" "$4}' > "${tfile}.user_block"
+sed -n '/Group\ quota\ on/q;p' "${tfile}.inode" | tail -n +5 | awk '{print $1" "$2" "$3" "$4}' > "${tfile}.user_inode"
 
 for u in ${users[@]}
 do
@@ -17,9 +17,9 @@ do
         echo "xfs_quota,path=${path},user=${u} kb_used=${kb_used},kb_soft=${kb_soft},kb_hard=${kb_hard},inode_used=${inode_used},inode_soft=${inode_soft},inode_hard=${inode_hard}"
 done
 
-groups=($(cat "${tfile}.block" | sed -n '/Group\ quota\ on/,$p' | awk '{print $1}' | tail -n +5 | xargs))
-cat "${tfile}.block" |  sed -n '/Group\ quota\ on/,$p' | tail -n +5 | awk '{print $1" "$2" "$3" "$4}' > "${tfile}.group_block"
-cat "${tfile}.inode" | sed -n '/Group\ quota\ on/,$p' | tail -n +5 | awk '{print $1" "$2" "$3" "$4}' > "${tfile}.group_inode"
+groups=($(sed -n '/Group\ quota\ on/,$p' "${tfile}.block" | awk '{print $1}' | tail -n +5 | xargs))
+sed -n '/Group\ quota\ on/,$p' "${tfile}.block" | tail -n +5 | awk '{print $1" "$2" "$3" "$4}' > "${tfile}.group_block"
+sed -n '/Group\ quota\ on/,$p' "${tfile}.inode" | tail -n +5 | awk '{print $1" "$2" "$3" "$4}' > "${tfile}.group_inode"
 for g in ${groups[@]}
 do
         IFS=" " read -r kb_used kb_soft kb_hard <<< $(awk -v group=$g '$1 == group {print $2" "$3" "$4}' "${tfile}.group_block")
